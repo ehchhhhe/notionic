2661ec42c6f136e662310cf9e9c2eb15da12a3a3
ehchhhhe <fliegen-bird@outlook.com>
2025-09-19 00:00:57 +0800

    更新博客配置，修改标题、作者信息及描述，调整语言和社交链接

--- blog.config.js
+++ blog.config.js
@@ -1,82 +1,82 @@
 const BLOG = {
-  title: 'Notionic',
-  author: '左蓝',
-  email: 'i@zuolan.me',
-  link: 'https://zuolan.me',
-  newsletter: 'Notionic Weekly',
-  description: 'A static blog build on top of Notion and Next.js',
-  lang: 'en-US', // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
-  timezone: 'Asia/Shanghai', // See https://en.wikipedia.org/wiki/List_of_tz_database_time_zones for all options.
-  appearance: 'auto', // ['light', 'dark', 'auto'],
-  font: 'sans-serif', // ['sans-serif', 'serif']
-  lightBackground: '#F6F8FA', // use hex value, don't forget '#' e.g #fffefc
-  darkBackground: '#212936', // use hex value, don't forget '#'
-  path: '', // leave this empty unless you want to deploy Notionic in a folder
-  since: 2022, // If leave this empty, current year will be used.
-  postsPerPage: 10,
-  sortByDate: true,
-  pagesShow: {
-    newsletter: true,
-    notes: true,
-    projects: true,
-    contact: true,
-    books: true,
-    friends: true
+  title: '鸡翅の小窝', // 博客标题
+  author: '鸡翅', // 作者名称
+  email: '1159502243@qq.com', // 作者邮箱
+  link: 'https://gfeather.xyz', // 博客链接
+  newsletter: 'Gfeather Weekly', // 新闻通讯名称
+  description: 'gfeather的数字生活手账。随手记录打游戏的快乐瞬间、生活里的琐碎趣事，以及那些让效率飙升的小工具。欢迎你来玩！', // 博客描述：
+  lang: 'zh-CN', // 语言设置：['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES']
+  timezone: 'Asia/Shanghai', // 时区设置：亚洲/上海，参见 https://en.wikipedia.org/wiki/List_of_tz_database_time_zones 获取所有选项
+  appearance: 'auto', // 外观模式：['light', 'dark', 'auto'] - 亮色、暗色、自动
+  font: 'sans-serif', // 字体设置：['sans-serif', 'serif'] - 无衬线字体、衬线字体
+  lightBackground: '#F6F8FA', // 亮色模式背景色：使用十六进制值，不要忘记'#'，例如#fffefc
+  darkBackground: '#212936', // 暗色模式背景色：使用十六进制值，不要忘记'#'
+  path: '', // 路径：除非你想将Notionic部署在子文件夹中，否则留空
+  since: 2022, // 博客成立年份：如果留空，将使用当前年份
+  postsPerPage: 10, // 每页显示文章数量
+  sortByDate: true, // 按日期排序文章
+  pagesShow: { // 显示的页面配置
+    newsletter: true, // 显示新闻通讯页面
+    notes: true, // 显示笔记页面
+    projects: true, // 显示项目页面
+    contact: true, // 显示联系页面
+    books: true, // 显示书籍页面
+    friends: true // 显示友链页面
   },
-  showWeChatPay: true,
-  previewImagesEnabled: true,
-  autoCollapsedNavBar: false, // The automatically collapsed navigation bar
-  ogImageGenerateHost: 'og-zl.vercel.app', // The link to generate OG image, don't end with a slash
-  defaultCover: '/cover.jpg',
-  socialLink: {
-    twitter: 'https://twitter.com/izuolan',
-    github: 'https://github.com/izuolan',
-    telegram: 'https://t.me/zuolan'
+  showWeChatPay: true, // 显示微信支付赞赏
+  previewImagesEnabled: true, // 启用预览图片
+  autoCollapsedNavBar: false, // 自动折叠导航栏
+  ogImageGenerateHost: 'og-zl.vercel.app', // 生成OG图片的链接，不要以斜杠结尾
+  defaultCover: '/cover.jpg', // 默认封面图片
+  socialLink: { // 社交媒体链接
+    twitter: 'https://twitter.com/izuolan', // Twitter链接
+    github: 'https://github.com/izuolan', // GitHub链接
+    telegram: 'https://t.me/zuolan' // Telegram链接
   },
-  seo: {
-    keywords: ['Notionic', 'Zuolan', 'Blog'],
-    googleSiteVerification: '' // Remove the value or replace it with your own google site verification code
+  seo: { // SEO配置
+    keywords: ['gfeather' , 'Blog'], // 关键词
+    googleSiteVerification: '' // 移除该值或替换为你自己的Google站点验证代码
   },
-  notionPageId: process.env.NOTION_PAGE_ID, // DO NOT CHANGE THIS! Edit .env file!
-  notionSpacesId: process.env.NOTION_SPACES_ID, // DO NOT CHANGE THIS! Edit .env file!
-  notionAccessToken: process.env.NOTION_ACCESS_TOKEN, // Useful if you prefer not to make your database public
-  notionDomain: 'izuolan.notion.site',
-  telegramToken: process.env.TELEGRAM_TOKEN, // The token of your Telegram bot
-  telegramChatId: '263895784', // The chat id of your Telegram bot
-  telegramChannelUrl: 'https://channel.zuolan.me/', // The link of your Telegram channel
-  telegramChannelName: 'zuolan_me', // The name of your Telegram channel
-  craftConfigShareUrl: 'https://www.craft.do/s/kQtcWqkv98cHhB', // The link to share your craft config
-  analytics: {
-    provider: '', // Currently we support Google Analytics, Ackee, Umami and Cloudflare Insights, please fill with 'ga' or 'ackee' or 'umami' or 'cf', leave it empty to disable it.
-    ackeeConfig: {
-      tracker: '', // e.g 'https://ackee.example.com/tracker.js'
-      dataAckeeServer: '', // e.g https://ackee.example.com , don't end with a slash
-      domainId: '' // e.g '0e2257a8-54d4-4847-91a1-0311ea48cc7b'
+  notionPageId: process.env.NOTION_PAGE_ID, // Notion页面ID：不要修改！编辑.env文件！
+  notionSpacesId: process.env.NOTION_SPACES_ID, // Notion空间ID：不要修改！编辑.env文件！
+  notionAccessToken: process.env.NOTION_ACCESS_TOKEN, // Notion访问令牌：如果你不想公开数据库，这个很有用
+  notionDomain: 'gfeather9527.notion.site', // Notion域名
+  telegramToken: process.env.TELEGRAM_TOKEN, // Telegram机器人令牌
+  telegramChatId: '263895784', // Telegram机器人聊天ID
+  telegramChannelUrl: 'https://channel.zuolan.me/', // Telegram频道链接
+  telegramChannelName: 'zuolan_me', // Telegram频道名称
+  craftConfigShareUrl: 'https://www.craft.do/s/kQtcWqkv98cHhB', // Craft配置分享链接
+  analytics: { // 分析工具配置
+    provider: 'ga', // 目前支持Google Analytics、Ackee、Umami和Cloudflare Insights，请填写'ga'、'ackee'、'umami'或'cf'，留空则禁用
+    ackeeConfig: { // Ackee配置
+      tracker: '', // 例如 'https://ackee.example.com/tracker.js'
+      dataAckeeServer: '', // 例如 https://ackee.example.com ，不要以斜杠结尾
+      domainId: '' // 例如 '0e2257a8-54d4-4847-91a1-0311ea48cc7b'
     },
-    cfConfig: {
-      scriptUrl: 'https://static.cloudflareinsights.com/beacon.min.js', // Default
-      token: '' // Like '{"token": "xxxxxxxxxxxxxxxxxx"}'
+    cfConfig: { // Cloudflare配置
+      scriptUrl: 'https://static.cloudflareinsights.com/beacon.min.js', // 默认
+      token: '' // 类似 '{"token": "xxxxxxxxxxxxxxxxxx"}'
     },
-    gaConfig: {
-      measurementId: '' // e.g: G-XXXXXXXXXX
+    gaConfig: { // Google Analytics配置
+      measurementId: 'G-S88938VTGC' // 例如: G-XXXXXXXXXX
     },
-    umamiConfig: {
-      scriptUrl: '', // The url of your Umami script
-      websiteId: '' // The website id of your Umami instance
+    umamiConfig: { // Umami配置
+      scriptUrl: '', // 你的Umami脚本URL
+      websiteId: '' // 你的Umami实例的网站ID
     }
   },
-  comment: {
-    // support provider: utterances, supacomments
-    provider: '', // leave it empty if you don't need any comment plugin
-    supaCommentsConfig: {
-      supabaseUrl: '', // The url of your Supabase instance
-      supabaseAnonKey: '' // The anonymous key of your Supabase instance
+  comment: { // 评论系统配置
+    // 支持的提供商：utterances, supacomments
+    provider: '', // 如果不需要评论插件，请留空
+    supaCommentsConfig: { // SupaComments配置
+      supabaseUrl: '', // 你的Supabase实例URL
+      supabaseAnonKey: '' // 你的Supabase实例匿名密钥
     },
-    utterancesConfig: {
-      repo: ''
+    utterancesConfig: { // Utterances配置
+      repo: '' // GitHub仓库名称
     }
   },
-  isProd: process.env.VERCEL_ENV === 'production' // distinguish between development and production environment (ref: https://vercel.com/docs/environment-variables#system-environment-variables)
+  isProd: process.env.VERCEL_ENV === 'production' // 区分开发和生产环境（参考：https://vercel.com/docs/environment-variables#system-environment-variables）
 }
 // export default BLOG
 module.exports = BLOG
